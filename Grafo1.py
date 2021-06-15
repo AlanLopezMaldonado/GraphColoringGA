49c9f841cc260bc3d5e6d1c7ca66470f72a24524
Alan Lopez <alanlopez_arlm@hotmail.com>
2021-06-14 19:44:48 -0500

    Comentarios en Grafo1.py

--- Grafo1.py
+++ Grafo1.py
@@ -4,8 +4,15 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import pylab
 
-
-
+"""
+FUNCION PARA LEER EL NOMBRE DE LOS VERTICES DEL GRAFO
+Esta funcion lee un archivo de texto que contiene escrito al grafo y obtiene el nombre de los vertices
+Esta funcion unicamente lee la primera linea del archivo de texto correspondiente al grafo(.txt)
+ya que este debe tener enlistado en su primera linea (y separado por comas) el nombre de los vertices.
+por ejemplo:
+vertice1,vertice2,vertice3,vertice4 
+y asi segun corresponda al grafo.
+"""
 def ObtenerVerticesGrafo(nombre):
     lista=[]
     arch = open(nombre, 'r')
@@ -15,8 +22,18 @@ def ObtenerVerticesGrafo(nombre):
     lista.pop(len(lista)-1)
     return lista
 
-
-
+"""
+FUNCION PARA LEER LAS ADYACENCIAS DEL GRAFO
+Esta funcion continua leyendo el archivo .txt correspondiente al grafo
+a partir de la segunda linea del archivo de texto, se enlistan las adyacencias de los vertices
+Por ejemplo si tenemos:
+vertice1,vertice3
+vertice1,vertice5
+vertice2,vertice4
+etc
+el vertice1 tiene dyacencia con el vertice3, el vertice1 tiene adyacencia con el vertice5 
+y asi segun corresponda al grafo.
+"""
 def ObtenerAdyacenciaGrafo(nombre):  
     lista=[]
     pardevertices=[]
@@ -32,8 +49,12 @@ def ObtenerAdyacenciaGrafo(nombre):
     return lista
 
 
-
-
+"""
+FUNCION PARA CONSTRUIR Y REPRESENTAR AL GRAFO
+Esta funcion recibe dos listas: una correspondiente al nombre de lso vertices o nodos y 
+la otra correspondiente a las adyacencias del grafo, y usa networkx para construir
+la estructura del grafo y representar de manera visual el grafo construido a partir del archivo .txt
+"""
 def ConstruirGrafo(listaNodos, ADYACENCIA):
     G=nx.Graph()
     G.add_nodes_from(listaNodos)
@@ -59,12 +80,16 @@ def ConstruirGrafo(listaNodos, ADYACENCIA):
     
 
 
-
+"""
+FUNCION PARA INCIAR LA CONSTRUCCION Y REPRESENTACION DEL GRAFO
+se recibe como parametro el nombre del grafo y se llama a las funciones
+descritas anteriormenet para crear al grafo.
+"""
 def GrafoNetworkx(NOMBREGRAFO): 
     Nodos=ObtenerVerticesGrafo(NOMBREGRAFO)
     NodosAdyacentes=ObtenerAdyacenciaGrafo(NOMBREGRAFO)
-    print Nodos
-    print NodosAdyacentes
+    #print Nodos          #no afecta funcionalidad. solo es para ver que esta ocurriendo
+    #print NodosAdyacentes #no afecta funcionalidad
     ConstruirGrafo(Nodos, NodosAdyacentes)
     
 
